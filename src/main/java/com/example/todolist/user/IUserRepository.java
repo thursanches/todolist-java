314aa7ccb724ce6fdbb4906b55a44bd541ed0b8a
Arthur Soares <arthursks7@gmail.com>
2023-10-16 15:16:36 -0300

    DB integration

---
+++ src/main/java/com/example/todolist/user/IUserRepository.java
@@ -0,0 +1,10 @@
+package com.example.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+  UserModel findByUsername(String username);
+  
+}
